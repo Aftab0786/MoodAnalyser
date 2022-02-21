52aa22ede9d2d68da93ccece0ac222aa605873ef
Aftab-1998 <alam41976@gmail.com>
2022-02-21 07:26:23 +0530

    [AFTAB3] Add; Added the method of invalid mood

--- MoodAnalyserProblem/Mood.cs
+++ MoodAnalyserProblem/Mood.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoodAnalyserProblems;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,17 +21,18 @@ namespace MoodAnalyserProblem
         //Method to analyse the mood from  the given message
         public string AnalyseMood()
         {
-            //Handling exception if user provide null value(UC2)
+            //Handling exception if user provide null or empty value(UC2&UC3)
             try
             {
-                if (message.ToLower().Contains("sad"))
-                {
+                //In case of null or empty mood throw custom exception MoodAnalysisException(UC3)
+                if (this.message.Equals(null))
+                    throw new MoodAnalysisException(MoodAnalysisException.ExceptionTypes.NULL_MOOD_EXCEPTION, "Message should not be null");
+                else if (this.message.Equals(string.Empty))
+                    throw new MoodAnalysisException(MoodAnalysisException.ExceptionTypes.EMPTY_MOOD_EXCEPTION, "Message should not be empty");
+                else if (message.ToLower().Contains("sad"))
                     return "sad";
-                }
                 else
-                {
                     return "happy";
-                }
             }
             catch (NullReferenceException)
             {
